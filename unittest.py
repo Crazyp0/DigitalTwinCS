7e9a2541c00c71e33727f495fb243c1d426624e7
MarineRobinson <90897781+MarineRobinson@users.noreply.github.com>
2021-09-28 10:08:14 +0200

    feat: room dimension test
    Successful test

--- unittest.py
+++ unittest.py
@@ -1,10 +1,13 @@
 from room import Room
+#from element import Element
 import unittest
 
-class RoomTest(unittest.TestCase):
-    def test_area(self):
+class DigitalTest(unittest.TestCase):
+    def test_room(self):
         Bedroom=Room(100,50,0,0)
         self.assertEqual(Bedroom.area(),5000)
+        self.assertEqual(Bedroom.largeur,50)
+    
 
 if __name__ == '__main__':
     unittest.main()
