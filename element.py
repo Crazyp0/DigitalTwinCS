0be47688a509ca41b0397a234ff6bdcb6ae55a4f
Crazyp0 <81258382+Crazyp0@users.noreply.github.com>
2021-09-28 10:07:29 +0200

    feat : add element and wall

---
+++ element.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 28 09:53:23 2021
+
+@author: matth
+"""
+
+class Element:
+    def __init__(self,position,rotation,longueur):
+        self.position=position
+        self.rotation=rotation
+        self.longueur=longueur
---
+++ wall.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 28 10:03:36 2021
+
+@author: matth
+"""
+from element import Element
+
+class Wall(Element):
+    def __init__(self,position,rotation,longueur):
+        super().__init__(self,position,rotation,longueur)
+    
