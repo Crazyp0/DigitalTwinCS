87cff497ef70d0c1c22e77156ba0963f453dd2f1
Crazyp0 <81258382+Crazyp0@users.noreply.github.com>
2021-10-07 11:46:28 +0200

    feat : add print on error

--- floor.py
+++ floor.py
@@ -14,9 +14,11 @@ class Floor:
         
     def addRoom(self,room):
         if(room.x+room.largeur>self.largeur or room.y+room.longueur>self.longueur):
+            print("room too large")
             return
         for r in self.rooms:
-            if(!(room.x>r.x+r.largeur or room.x+room.largeur<r.x or room.y>r.y+r.longueur or room.y+room.longueur<r.y )):
+            if((room.x>r.x+r.largeur or room.x+room.largeur<r.x or room.y>r.y+r.longueur or room.y+room.longueur<r.y)==False):
+                print("impossible to add this room")
                 return
         self.rooms.append(room)
     
