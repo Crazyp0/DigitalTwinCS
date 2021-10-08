1dc83263a67c3fa05aea72537255556418f4305d
Crazyp0 <81258382+Crazyp0@users.noreply.github.com>
2021-10-08 13:54:02 +0200

    feat : addDoor in Room

--- room.py
+++ room.py
@@ -4,8 +4,15 @@ Created on Tue Sep 28 08:39:47 2021
 
 @author: matth
 """
+from enum import Enum
 from wall import Wall
 import numpy as np
+
+class Direction(Enum):
+    SOUTH=0
+    WEST=1
+    NORTH=2
+    EST=3
 
 class Room:
     def __init__(self,longueur,largeur,x,y,name):
@@ -15,19 +22,23 @@ class Room:
         self.longueur=longueur
         self.x=x
         self.y=y
+        self.listOfWall=[]
         if(len(name)>self.maxNbCharacter):
             self.name=name[:self.maxNbCharacter]
         else:
             self.name=name
     
     def generateWall(self):
-        list_of_wall=[]
-        list_of_wall.append(Wall((self.x,self.y),0,self.largeur))
-        list_of_wall.append(Wall((self.x,self.y),np.pi/2,self.longueur))
-        list_of_wall.append(Wall((self.x,self.y+self.longueur),0,self.largeur))
-        list_of_wall.append(Wall((self.x+self.largeur,self.y+self.longueur),-np.pi/2,self.longueur))
-        return list_of_wall
-        
+        self.list_of_wall.append(Wall((self.x,self.y),0,self.largeur))
+        self.list_of_wall.append(Wall((self.x,self.y),np.pi/2,self.longueur))
+        self.list_of_wall.append(Wall((self.x,self.y+self.longueur),0,self.largeur))
+        self.list_of_wall.append(Wall((self.x+self.largeur,self.y+self.longueur),-np.pi/2,self.longueur))
+        return self.list_of_wall
+    
+    def addDoorOnAWall(self,positionOnWall,longueurDoor,direction):
+        if(self.listOfWall[direction]!=None):
+            self.listOfWall[direction].addDoorOnAWall(positionOnWall,longueurDoor)
+          
     def area(self):
         return self.longueur*self.largeur
         
